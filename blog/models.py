4be7e14980fb46a2a465e2f81cc2ba6703cb020a
matr369 <matr369@gmail.com>
2014-04-14 07:41:01 +0300

    1
    1

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib import admin
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    timestamp = models.DateTimeField()
+    
+admin.site.register(BlogPost)
